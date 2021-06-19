faa7511e13d6b0032596fd33020465e9e180379a
wangkui0508 <wangkui0508@hotmail.com>
2021-06-19 13:05:09 +0800

    Add progress report for recoverTree

--- datatree/load_dump.go
+++ datatree/load_dump.go
@@ -339,11 +339,19 @@ func (tree *Tree) RecoverEntry(pos int64, entry *Entry, deactivedSNList []int64,
 func (tree *Tree) ScanEntries(oldestActiveTwigID int64, outChan chan types.EntryX) {
 	pos := tree.twigMtFile.GetFirstEntryPos(oldestActiveTwigID)
 	size := tree.entryFile.Size()
+	total := size - pos
+	step := total / 20
+	startPos := pos
+	lastPos := pos
 	for pos < size {
 		//!! if pos > 108995312 {
 		//!! 	entryBz, nxt := tree.entryFile.ReadEntryRawBytes(pos)
 		//!! 	fmt.Printf("Fuck now pos %d %#v len=%d nxt=%d\n", pos, entryBz, len(entryBz), nxt)
 		//!! }
+		if (pos-startPos)/step != (lastPos-startPos)/step {
+			fmt.Printf("ScanEntries %1.2f %d/%d\n", float64(pos-startPos)/float64(total), pos-startPos, total)
+		}
+		lastPos = pos
 		key, deactivedSNList, nextPos := tree.entryFile.ReadEntryAndSNList(pos)
 		outChan <- types.EntryX{Entry: key, Pos: pos, DeactivedSNList: deactivedSNList}
 		pos = nextPos
@@ -354,7 +362,15 @@ func (tree *Tree) ScanEntries(oldestActiveTwigID int64, outChan chan types.Entry
 func (tree *Tree) ScanEntriesLite(oldestActiveTwigID int64, outChan chan types.KeyAndPos) {
 	pos := tree.twigMtFile.GetFirstEntryPos(oldestActiveTwigID)
 	size := tree.entryFile.Size()
+	total := size - pos
+	step := total / 20
+	startPos := pos
+	lastPos := pos
 	for pos < size {
+		if (pos-startPos)/step != (lastPos-startPos)/step {
+			fmt.Printf("ScanEntriesLite %1.2f %d/%d\n", float64(pos-startPos)/float64(total), pos-startPos, total)
+		}
+		lastPos = pos
 		entryBz, next := tree.entryFile.ReadEntryRawBytes(pos)
 		outChan <- types.KeyAndPos{
 			Key:       ExtractKeyFromRawBytes(entryBz),
